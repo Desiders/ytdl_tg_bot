fc30d9b40e79022c38ed14be56cd0748ad13e635
Desiders <abc_desi@proton.me>
2023-10-26 01:29:50 +0300

    Add start handler

---
+++ src/handlers/start.rs
@@ -0,0 +1,20 @@
+use telers::{
+    event::{telegram::HandlerResult, EventReturn},
+    methods::SendMessage,
+    types::Message,
+    Bot,
+};
+
+pub async fn start(bot: Bot, message: Message) -> HandlerResult {
+    let text = format!(
+        "Hello, {first_name}!\n\nSend me a link to a YouTube video and I'll send you the video file!",
+        first_name = message
+            .from.as_ref()
+            .map_or("Anonymous", |user| user.first_name.as_str()),
+    );
+
+    bot.send(&SendMessage::new(message.chat_id(), text), None)
+        .await?;
+
+    Ok(EventReturn::Finish)
+}
