0b800b2e5852db4bc13471ccd04c3a10a648617e
Desiders <abc_desi@proton.me>
2023-10-26 01:25:40 +0300

    Add check url filter

---
+++ src/filters.rs
@@ -0,0 +1,3 @@
+mod is_correct_url;
+
+pub use is_correct_url::is_correct_url;
---
+++ src/filters/is_correct_url.rs
@@ -0,0 +1,25 @@
+use std::future::Future;
+use telers::{types::Update, Bot, Context};
+use tracing::{event, Level};
+use url::Url;
+
+pub fn is_correct_url(
+    _bot: &Bot,
+    update: &Update,
+    _context: &Context,
+) -> impl Future<Output = bool> {
+    let result = if let Some(text) = update.text() {
+        match Url::parse(text.trim()) {
+            Ok(_) => true,
+            Err(err) => {
+                event!(Level::DEBUG, %err, "Error while parsing url");
+
+                false
+            }
+        }
+    } else {
+        false
+    };
+
+    async move { result }
+}
