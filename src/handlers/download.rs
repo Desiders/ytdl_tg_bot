92a01a6446e9067fd94ecd36eea5e10574d9c429
Desiders <abc_desi@proton.me>
2024-07-15 00:42:49 +0300

    Upgrade dependencies

--- src/handlers/download.rs
+++ src/handlers/download.rs
@@ -30,7 +30,7 @@ const DOWNLOAD_MEDIA_TIMEOUT: u64 = 45;
 const SEND_VIDEO_TIMEOUT: f32 = 30.0;
 const SEND_AUDIO_TIMEOUT: f32 = 30.0;
 const GET_MEDIA_OR_PLAYLIST_INFO_INLINE_QUERY_TIMEOUT: u64 = 12;
-const SELECT_INLINE_QUERY_CACHE_TIME: i64 = 3600; // 60 minutes
+const SELECT_INLINE_QUERY_CACHE_TIME: i64 = 86400; // 24 hours
 
 #[derive(thiserror::Error, Debug)]
 pub enum DownloadErrorKind {
