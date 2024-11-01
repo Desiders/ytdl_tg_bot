c19b1bbb15fd63b440b37416fa10fbad512747d5
Desiders <abc_desi@proton.me>
2024-11-01 15:39:51 +0300

    Upgrade `telers` version. Refactor inline download

--- src/filters.rs
+++ src/filters.rs
@@ -1,3 +1,5 @@
 mod text_contains_url;
+mod via_bot;
 
 pub use text_contains_url::{text_contains_url, text_contains_url_with_reply};
+pub use via_bot::is_via_bot;
--- src/models.rs
+++ src/models.rs
@@ -4,6 +4,4 @@ pub mod format;
 pub mod video;
 
 pub use audio::{AudioInFS, TgAudioInPlaylist};
-pub use combined_format::{Format as CombinedFormat, Formats as CombinedFormats};
-pub use format::{Audio as AudioFormat, Video as VideoFormat};
 pub use video::{TgVideoInPlaylist, VideoInFS, VideoInYT, VideosInYT};
