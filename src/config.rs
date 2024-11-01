42563c33cf0e6bc4265af051e954816283a417e1
Desiders <abc_desi@proton.me>
2024-11-01 15:49:39 +0300

    Remove unused phantom data

--- src/config.rs
+++ src/config.rs
@@ -2,52 +2,14 @@ use std::{
     borrow::Cow,
     env::{self, VarError},
     num::ParseIntError,
-    ops::Deref,
     str::ParseBoolError,
 };
-use telers::extractors::FromContext;
 
 #[derive(Clone, Debug)]
 pub struct Bot {
     pub token: String,
     pub source_code_url: String,
     pub receiver_video_chat_id: i64,
-}
-
-#[derive(Clone, Debug, FromContext)]
-#[context(key = "phantom_video_id")]
-pub struct PhantomVideoId(pub String);
-
-impl Deref for PhantomVideoId {
-    type Target = String;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-#[derive(Clone, Debug, FromContext)]
-#[context(key = "phantom_audio_id")]
-pub struct PhantomAudioId(pub String);
-
-impl Deref for PhantomAudioId {
-    type Target = String;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-#[derive(Clone, Debug)]
-pub enum PhantomVideo {
-    Id(PhantomVideoId),
-    Path(String),
-}
-
-#[derive(Clone, Debug)]
-pub enum PhantomAudio {
-    Id(PhantomAudioId),
-    Path(String),
 }
 
 #[derive(Clone, Debug)]
@@ -59,8 +21,6 @@ pub struct YtDlp {
 #[derive(Clone, Debug)]
 pub struct Config {
     pub bot: Bot,
-    pub phantom_video: PhantomVideo,
-    pub phantom_audio: PhantomAudio,
     pub yt_dlp: YtDlp,
 }
 
@@ -75,38 +35,6 @@ pub enum ErrorKind {
 }
 
 pub fn read_config_from_env() -> Result<Config, ErrorKind> {
-    let phantom_video_id_env = env::var("PHANTOM_VIDEO_ID");
-    let phantom_video_path = env::var("PHANTOM_VIDEO_PATH");
-
-    let phantom_video = if phantom_video_id_env.is_ok() && !phantom_video_id_env.as_ref().unwrap().is_empty() {
-        #[allow(clippy::unnecessary_unwrap)]
-        PhantomVideo::Id(PhantomVideoId(phantom_video_id_env.unwrap()))
-    } else if phantom_video_path.is_ok() && !phantom_video_path.as_ref().unwrap().is_empty() {
-        #[allow(clippy::unnecessary_unwrap)]
-        PhantomVideo::Path(phantom_video_path.unwrap())
-    } else {
-        return Err(ErrorKind::Env {
-            source: VarError::NotPresent,
-            key: "PHANTOM_VIDEO_ID or PHANTOM_VIDEO_PATH".into(),
-        });
-    };
-
-    let phantom_audio_id_env = env::var("PHANTOM_AUDIO_ID");
-    let phantom_audio_path = env::var("PHANTOM_AUDIO_PATH");
-
-    let phantom_audio = if phantom_audio_id_env.is_ok() && !phantom_audio_id_env.as_ref().unwrap().is_empty() {
-        #[allow(clippy::unnecessary_unwrap)]
-        PhantomAudio::Id(PhantomAudioId(phantom_audio_id_env.unwrap()))
-    } else if phantom_audio_path.is_ok() && !phantom_audio_path.as_ref().unwrap().is_empty() {
-        #[allow(clippy::unnecessary_unwrap)]
-        PhantomAudio::Path(phantom_audio_path.unwrap())
-    } else {
-        return Err(ErrorKind::Env {
-            source: VarError::NotPresent,
-            key: "PHANTOM_AUDIO_ID or PHANTOM_AUDIO_PATH".into(),
-        });
-    };
-
     Ok(Config {
         bot: Bot {
             token: env::var("BOT_TOKEN").map_err(|err| ErrorKind::Env {
@@ -125,8 +53,6 @@ pub fn read_config_from_env() -> Result<Config, ErrorKind> {
                 .parse()
                 .map_err(ErrorKind::ParseInt)?,
         },
-        phantom_video,
-        phantom_audio,
         yt_dlp: YtDlp {
             full_path: env::var("YT_DLP_FULL_PATH").map_err(|err| ErrorKind::Env {
                 source: err,
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,5 @@
-mod phantom_audio;
-mod phantom_video;
 mod shutdown;
 mod startup;
 
-pub use phantom_audio::get_phantom_audio_id;
-pub use phantom_video::get_phantom_video_id;
 pub use shutdown::on_shutdown;
 pub use startup::on_startup;
