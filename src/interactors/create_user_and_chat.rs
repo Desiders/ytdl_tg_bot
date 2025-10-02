f52a9e0639aead606feb6ddfb7cdb7a17dca0a30
Desiders <47452083+Desiders@users.noreply.github.com>
2025-10-03 00:16:27 +0300

    Add create user and chat interactor

---
+++ src/interactors/create_user_and_chat.rs
@@ -0,0 +1,62 @@
+use std::convert::Infallible;
+use tracing::{event, instrument, Level};
+
+use super::Interactor;
+use crate::{
+    database::TxManager,
+    entities::{Chat, User},
+    errors::database::ErrorKind,
+};
+
+pub struct CreateUserAndChat<'a> {
+    pub tx_manager: &'a mut TxManager,
+}
+
+impl<'a> CreateUserAndChat<'a> {
+    pub const fn new(tx_manager: &'a mut TxManager) -> Self {
+        Self { tx_manager }
+    }
+}
+
+pub struct CreateUserAndChatInput {
+    pub user: User,
+    pub chat: Chat,
+}
+
+pub struct CreateUserAndChatOutput {
+    pub user: User,
+    pub chat: Chat,
+}
+
+impl<'a> Interactor for CreateUserAndChat<'a> {
+    type Input = CreateUserAndChatInput;
+    type Output = CreateUserAndChatOutput;
+    type Err = ErrorKind<Infallible>;
+
+    #[instrument(skip(self))]
+    async fn execute(&mut self, Self::Input { user, chat }: Self::Input) -> Result<Self::Output, Self::Err> {
+        self.tx_manager.begin().await?;
+
+        let user = match self.tx_manager.user_dao()?.insert_or_update(user).await {
+            Ok(val) => val,
+            Err(err) => {
+                self.tx_manager.rollback().await?;
+                return Err(err.into());
+            }
+        };
+        event!(Level::INFO, "User created");
+
+        let chat = match self.tx_manager.chat_dao()?.insert_or_update(chat).await {
+            Ok(val) => val,
+            Err(err) => {
+                self.tx_manager.rollback().await?;
+                return Err(err.into());
+            }
+        };
+        event!(Level::INFO, "Chat created");
+
+        self.tx_manager.commit().await?;
+
+        Ok(Self::Output { user, chat })
+    }
+}
