b38b85b7ab66da5ca1ea62db47820155e3b08d15
Desiders <7452083+Desiders@users.noreply.github.com>
2025-10-04 21:14:03 +0300

    Add aspect related utils re-export

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,13 @@
 mod error;
 mod shutdown;
 mod startup;
+mod thumbnail;
 mod url;
+mod video;
 
 pub use error::format_error_report;
 pub use shutdown::on_shutdown;
 pub use startup::on_startup;
+pub use thumbnail::get_url_by_aspect;
 pub use url::{get_video_id, ErrorKind as GetVideoIdErrorKind};
+pub use video::{calculate_aspect_ratio, get_nearest_to_aspect, AspectKind};
