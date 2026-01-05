0df2f35b896a7bec621091aafd4381bbe58407c2
literally user <literally_user@hotmail.com>
2026-01-05 15:49:37 +0300

    Add some test cases

--- src/utils.rs
+++ src/utils.rs
@@ -4,6 +4,7 @@ mod startup;
 mod thumbnail;
 mod url;
 mod video;
+mod fs;
 
 pub use error::{format_error_report, FormatErrorToMessage};
 pub use shutdown::on_shutdown;
@@ -11,3 +12,4 @@ pub use startup::on_startup;
 pub use thumbnail::get_urls_by_aspect;
 pub use url::{get_video_id, ErrorKind as GetVideoIdErrorKind};
 pub use video::{calculate_aspect_ratio, get_nearest_to_aspect, AspectKind};
+pub use fs::sanitize_send_filename;
