76e48fca6778287bad217c12b4a9f482ca0dd80c
Desiders <47452083+Desiders@users.noreply.github.com>
2026-01-12 18:54:06 +0300

    Add chat config table migration and generate models

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
 mod m20251113_100712_remove_chat_from_downloaded_media;
+mod m20260112_142259_chat_config;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20251113_100712_remove_chat_from_downloaded_media::Migration),
+            Box::new(m20260112_142259_chat_config::Migration),
         ]
     }
 }
