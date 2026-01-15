2c1add8039af82f2fefb51cd824cf68af6fe9ab0
Desiders <47452083+Desiders@users.noreply.github.com>
2026-01-15 13:31:25 +0300

    Move `parse_params` to utils

--- src/filters/text_url.rs
+++ src/filters/text_url.rs
@@ -1,39 +1,10 @@
-use crate::{config::BlacklistedConfig, entities::UrlWithParams};
+use crate::{config::BlacklistedConfig, entities::UrlWithParams, utils::parse_params};
 
 use froodi::async_impl::Container;
 use std::{collections::HashMap, future::Future, str::FromStr};
 use telers::{types::UpdateKind, Request};
 use tracing::error;
 use url::Url;
-
-fn parse_params(param_block: &str) -> Vec<(Box<str>, Box<str>)> {
-    let inner = param_block.trim();
-    // Remove the surrounding brackets if they exist.
-    let inner = inner.strip_prefix('[').and_then(|s| s.strip_suffix(']')).unwrap_or(inner);
-    let params = inner
-        .split(',')
-        .filter_map(|param| {
-            let trimmed = param.trim();
-            if trimmed.is_empty() {
-                None
-            } else {
-                let parts: Vec<&str> = trimmed.splitn(2, '=').collect();
-                if parts.len() == 2 {
-                    let key = parts[0].trim();
-                    let value = parts[1].trim();
-                    if key.is_empty() || value.is_empty() {
-                        None
-                    } else {
-                        Some((key.to_owned().into_boxed_str(), value.to_owned().into_boxed_str()))
-                    }
-                } else {
-                    None
-                }
-            }
-        })
-        .collect::<Vec<_>>();
-    params
-}
 
 /// Extracts a URL and its following parameters from the given text.
 ///
--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,17 @@
 mod error;
+mod fs;
+mod params;
 mod shutdown;
 mod startup;
 mod thumbnail;
 mod url;
 mod video;
-mod fs;
 
 pub use error::{format_error_report, FormatErrorToMessage};
+pub use fs::sanitize_send_filename;
+pub use params::parse_params;
 pub use shutdown::on_shutdown;
 pub use startup::on_startup;
 pub use thumbnail::get_urls_by_aspect;
 pub use url::{get_video_id, ErrorKind as GetVideoIdErrorKind};
 pub use video::{calculate_aspect_ratio, get_nearest_to_aspect, AspectKind};
-pub use fs::sanitize_send_filename;
