756184cfcfcd894f2fb3c8b64384dae2041b1eeb
Desiders <47452083+Desiders@users.noreply.github.com>
2026-01-31 05:00:09 +0300

    Update `start` command

--- src/handlers/start.rs
+++ src/handlers/start.rs
@@ -15,25 +15,35 @@ use tracing::instrument;
 pub async fn start(bot: Bot, message: Message, Inject(cfg): Inject<Config>) -> HandlerResult {
     let bot_info = bot.send(GetMe {}).await?;
     let text = format!(
-        "Hi, {first_name}. I'm a bot that can help you download media from YouTube and other resources.\n\n\
-        In a private chat, send me a video link and I will reply with a file.\n\
-        In a group chat, send <code>/vd</code> (<code>/video_download</code>) with a link or reply to a message containing a link.\n\
-        If you want to download audio, send <code>/ad</code> (<code>/audio_download</code>) instead of <code>/vd</code>.\n\
-        You can also use <code>/rv</code> (<code>/random_video</code>) or <code>/ra</code> (<code>/random_audio</code>) to get random media.\n\n\
-        Params to download several media from playlist: <code>[items=start:stop:step]</code>.\n\
-        Params to select audio language: <code>[lang=ru]</code>.\n\
-        Params to override used domains for <code>/rv</code> and <code>/ra</code>: <code>[domains=youtube.com|youtu.be]</code>.\n\n\
-        You can use me in inline mode in any chat by typing <code>@{bot_username} </code><code>&lt;url&gt;</code>.\n\
-        If text is specified instead of a URL, a YouTube video search will be performed.\n\
-        * I download videos and audios in the best available quality under {max_file_size_in_mb}MB.\n\
-        * The bot is open source, and you can find the source code: {source_code}.",
-        first_name = message
-            .from()
-            .as_ref()
-            .map_or("Anonymous".to_owned(), |user| html_quote(user.first_name.as_ref())),
+        "<b>Commands</b>\n\
+        - <code>/vd</code> — download video. Calling this command is required to display download progress; otherwise, downloading will occur in \"silent\" mode.\n\
+        - <code>/ad</code> — download audio\n\
+        - <code>/rv</code>, <code>/ra</code> — random video or audio\n\
+        \n\
+        <b>Inline Mode</b>\n\
+        - <code>@{bot_username} &lt;url&gt;</code> — download by link\n\
+        - <code>@{bot_username} &lt;title&gt;</code> — search on YouTube\n\
+        \n\
+        <b>Arguments</b>\n\
+        For <code>/vd</code> and <code>/ad</code>:\n\
+          - lang: Preferred audio language, example: <code>/vd [lang=ru]</code>\n\
+          - items: Playlist download, <code>start:end:step</code> (default: 1 for each argument, max: 10 media per command), example: <code>/vd [items=1:3:1]</code>\n\
+        \n\
+        For <code>/rv</code> and <code>/ra</code>:\n\
+          - domains: Sources separated by <code>|</code>, example: <code>/rv [domains=youtube.com|youtu.be]</code>\n\
+        \n\
+        <b>Notes</b>\n\
+        - Arguments are specified in square brackets separated by commas: <code>[arg=value,arg2=value]</code>;\n\
+        - All arguments are optional;\n\
+        - Thousands of websites are supported;\n\
+        - Inline mode supports <code>lang</code>, but doesn't support <code>items</code>;\n\
+        - You can add <code>yv2t_bot=false</code> to a link to ignore it;\n\
+        - I download media in the best quality under {max_file_size_in_mb}MB;\n\
+        - The bot is open source: {source_code}.\
+        ",
         bot_username = bot_info.username.expect("Bots always have a username"),
         max_file_size_in_mb = cfg.yt_dlp.max_file_size / 1000 / 1000,
-        source_code = html_text_link("here", html_quote(&cfg.bot.src_url)),
+        source_code = html_text_link("source code", html_quote(&cfg.bot.src_url)),
     );
 
     bot.send(
