95c5458f25139ac65bd643b8eb878ce19f3766c9
Desiders <47452083+Desiders@users.noreply.github.com>
2026-03-11 14:37:52 +0300

    Code changes for new `telers` version

--- src/filters/chosen_inline.rs
+++ src/filters/chosen_inline.rs
@@ -1,20 +1,20 @@
-use std::future::Future;
-use telers::Request;
+use std::{convert::Infallible, future::Future};
+use telers::{FilterResult, Request};
 
-pub fn is_video(request: &mut Request) -> impl Future<Output = bool> {
+pub fn is_video(request: &mut Request) -> impl Future<Output = FilterResult<Infallible>> {
     let result = if let Some(result_id) = request.update.result_id() {
         result_id.starts_with("video_")
     } else {
         false
     };
-    async move { result }
+    async move { Ok(result) }
 }
 
-pub fn is_audio(request: &mut Request) -> impl Future<Output = bool> {
+pub fn is_audio(request: &mut Request) -> impl Future<Output = FilterResult<Infallible>> {
     let result = if let Some(result_id) = request.update.result_id() {
         result_id.starts_with("audio_")
     } else {
         false
     };
-    async move { result }
+    async move { Ok(result) }
 }
