0305111f680f9aad9a8e8d78cac5262bed132c4c
Desiders <47452083+Desiders@users.noreply.github.com>
2026-06-14 03:45:16 +0300

    Start use `lofty` instead of `mutagen-rs` ro avoid OOM

--- downloader/src/services.rs
+++ downloader/src/services.rs
@@ -1,11 +1,11 @@
 pub mod domain_replacer;
 pub mod ffmpeg;
 pub mod gallery_dl;
-pub mod mutagen;
 pub mod spotdl;
+pub mod thumbnail;
 pub mod ytdl;
 
 pub use domain_replacer::DomainReplacer;
 pub use ffmpeg::download_and_convert;
-pub use mutagen::embed_thumbnail;
 pub use spotdl::SpotdlResolver;
+pub use thumbnail::embed_thumbnail;
